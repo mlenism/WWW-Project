b66d4a7301760875de7c2794b7b9733dfea80d83
alejandro1403 <65137750+alejandro1403@users.noreply.github.com>
2022-02-25 07:02:39 -0500

    Update Cedula.js

--- Fronted/src/pages/Cedula.js
+++ Fronted/src/pages/Cedula.js
@@ -1,13 +1,14 @@
 import { motion } from 'framer-motion';
 import { Link as RouterLink } from 'react-router-dom';
+import * as React from 'react';
 // material
 import { styled } from '@mui/material/styles';
 import { Box, Button, Typography, Container } from '@mui/material';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
-import ButtonGroup from '@mui/material/ButtonGroup';
 import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
+import BackspaceIcon from '@mui/icons-material/Backspace';
+import LoadingButton from '@mui/lab/LoadingButton';
 // components
 import { MotionContainer, varBounceIn } from '../components/animate';
 import Page from '../components/Page';
@@ -24,6 +25,8 @@ const RootStyle = styled(Page)(({ theme }) => ({
 // ----------------------------------------------------------------------
 
 export default function Cedula() {
+  const [completado, setCompletado] = React.useState(true);
+
   return (
     <RootStyle title="404 Page Not Found | Minimal-UI">
       <Container>
@@ -34,19 +37,68 @@ export default function Cedula() {
                 Digite su cedula
               </Typography>
             </motion.div>
-            <TextField fullWidth label="Cedula" id="cedula" />
-            <motion.div variants={varBounceIn}>
-              <Box
-                component="img"
-                src="/static/illustrations/illustration_404.svg"
-                sx={{ height: 260, mx: 'auto', my: { xs: 5, sm: 10 } }}
-              />
-            </motion.div>
+            <TextField
+              id="Cedula"
+              label="Cedula"
+              inputProps={{ inputMode: 'numeric' }}
+              checked={completado}
+              onChange={() => setCompletado(!completado)}
+            />
+            <Box sx={{ height: 260, mx: 'auto', my: { xs: 5, sm: 10 } }}>
+              <Grid>
+                <Button size="large" variant="outlined" value="1">
+                  1
+                </Button>
+                <Button size="large" variant="outlined">
+                  2
+                </Button>
+                <Button size="large" variant="outlined">
+                  3
+                </Button>
+              </Grid>
+              <Grid>
+                <Button size="large" variant="outlined">
+                  4
+                </Button>
+                <Button size="large" variant="outlined">
+                  5
+                </Button>
+                <Button size="large" variant="outlined">
+                  6
+                </Button>
+              </Grid>
+              <Grid>
+                <Button size="large" variant="outlined">
+                  7
+                </Button>
+                <Button size="large" variant="outlined">
+                  8
+                </Button>
+                <Button size="large" variant="outlined">
+                  9
+                </Button>
+              </Grid>
+              <Grid>
+                <Button size="large" variant="outlined" Width="50">
+                  0
+                </Button>
+                <div />
+                <Button size="large" variant="outlined" endIcon={<BackspaceIcon />}>
+                  Borrar
+                </Button>
+              </Grid>
+            </Box>
             <Stack direction="row" spacing={40}>
               <Button to="/solicitud" size="large" variant="contained" component={RouterLink}>
                 Atras
               </Button>
-              <Button to="/confirmado" size="large" variant="contained" component={RouterLink}>
+              <Button
+                to="/confirmado"
+                size="large"
+                variant="contained"
+                component={RouterLink}
+                disabled={completado}
+              >
                 Siguiente
               </Button>
             </Stack>
