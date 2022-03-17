2da571149b6003d9dc99b08835a2e6a6b0414096
alejandro1403 <65137750+alejandro1403@users.noreply.github.com>
2022-03-17 00:41:12 -0500

    Update Login.js

--- Fronted/src/pages/Login.js
+++ Fronted/src/pages/Login.js
@@ -1,16 +1,28 @@
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
+
+import eyeFill from '@iconify/icons-eva/eye-fill';
+import eyeOffFill from '@iconify/icons-eva/eye-off-fill';
+import { Icon } from '@iconify/react';
 // material
 import { styled } from '@mui/material/styles';
-import { Card, Stack, Link, Container, Typography } from '@mui/material';
+import {
+  Card,
+  Stack,
+  Link,
+  Container,
+  Typography,
+  Button,
+  TextField,
+  Box,
+  IconButton,
+  InputAdornment
+} from '@mui/material';
 // layouts
-import AuthLayout from '../layouts/AuthLayout';
 // components
 import Page from '../components/Page';
 import { MHidden } from '../components/@material-extend';
-import { LoginForm } from '../components/authentication/login';
-import AuthSocial from '../components/authentication/AuthSocial';
-import { getUsuarios, getUsuario, addUsuario } from '../apicore';
+import { login } from '../apicore';
 
 // ----------------------------------------------------------------------
 
@@ -42,50 +54,68 @@ const ContentStyle = styled('div')(({ theme }) => ({
 // ----------------------------------------------------------------------
 
 export default function Login() {
-  const agregarUsuario = async () => {
-    const data = {
-      username: 'user_name31',
-      first_name: 'first_name31',
-      last_name: 'last_name31',
-      email: 'email31@hotmail.com',
-      sede_nombre: 'sede2',
-      password: 'contrasenia'
-    };
+  const [credentials, setCredentials] = useState({});
+  const [showPassword, setShowPassword] = useState(false);
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
 
-    const response2 = await addUsuario(data);
+  const navigate = useNavigate();
 
-    console.log(response2);
+  const handleShowPassword = () => {
+    setShowPassword((show) => !show);
   };
+
+  const onChangeUsername = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const onChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
   useEffect(() => {
-    const response = getUsuarios();
-    console.log(response);
-    const response1 = getUsuario('1');
-    console.log(response1);
-    const data = {
-      username: 'user_name31',
-      first_name: 'first_name31',
-      last_name: 'last_name31',
-      email: 'email31@hotmail.com',
-      sede_nombre: 'sede2',
-      password: 'contrasenia'
+    const user = async () => {
+      const response = await login();
+      if (response !== undefined) setCredentials(response);
+      return response;
+    };
+    user();
+  }, []);
+
+  function validarUsuario(username, password) {
+    const user = {
+      token: '2be89b2448c7e6fc9fba85f76c8fd964a8e40987',
+      ser: {
+        username: 'admin',
+        first_name: 'ad',
+        last_name: 'min',
+        email: 'admin@admin.com',
+        is_superuser: username,
+        is_staff: password,
+        sede_nombre: null
+      },
+      message: 'Inicio de Sesión Exitoso.'
     };
 
-    agregarUsuario();
-  }, []);
+    if (Object.keys(user).length === 3) {
+      if (user.ser.is_staff === 'true' && user.ser.is_superuser === 'true') {
+        navigate('/dashboard');
+      } else if (user.ser.is_staff === 'true') {
+        navigate('/dashboard/products');
+      } else {
+        navigate('/solicitud');
+      }
+    } else {
+      navigate('');
+    }
+  }
 
   return (
     <RootStyle title="Login | Minimal-UI">
-      <AuthLayout>
-        Don’t have an account? &nbsp;
-        <Link underline="none" variant="subtitle2" component={RouterLink} to="/register">
-          Get started
-        </Link>
-      </AuthLayout>
-
       <MHidden width="mdDown">
         <SectionStyle>
           <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
-            Hi, Welcome Back
+            Bienvenido de vuelta
           </Typography>
           <img src="/static/illustrations/illustration_login.png" alt="login" />
         </SectionStyle>
@@ -95,22 +125,50 @@ export default function Login() {
         <ContentStyle>
           <Stack sx={{ mb: 5 }}>
             <Typography variant="h4" gutterBottom>
-              Sign in to Minimal
+              BTM
             </Typography>
-            <Typography sx={{ color: 'text.secondary' }}>Enter your details below.</Typography>
+            <Typography sx={{ color: 'text.secondary' }}>Ingresa tus datos.</Typography>
           </Stack>
-          <AuthSocial />
+          <Stack spacing={3}>
+            <TextField
+              fullWidth
+              autoComplete="username"
+              type="email"
+              label="Email address"
+              onChange={onChangeUsername}
+            />
 
-          <LoginForm />
-
-          <MHidden width="smUp">
-            <Typography variant="body2" align="center" sx={{ mt: 3 }}>
-              Don’t have an account?&nbsp;
-              <Link variant="subtitle2" component={RouterLink} to="register">
-                Get started
-              </Link>
-            </Typography>
-          </MHidden>
+            <TextField
+              fullWidth
+              autoComplete="current-password"
+              label="Password"
+              type={showPassword ? 'text' : 'password'}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={handleShowPassword} edge="end">
+                      <Icon icon={showPassword ? eyeFill : eyeOffFill} />
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }}
+              onChange={onChangePassword}
+            />
+          </Stack>
+          <Box
+            component="img"
+            src="/static/illustrations/illustration_404.svg"
+            sx={{ height: 260, mx: 'auto', my: { xs: 5, sm: 10 } }}
+          />
+          <Button
+            fullWidth
+            size="large"
+            type="submit"
+            variant="contained"
+            onClick={() => validarUsuario(username, password)}
+          >
+            Login
+          </Button>
         </ContentStyle>
       </Container>
     </RootStyle>
