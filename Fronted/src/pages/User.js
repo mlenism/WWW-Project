09eed1905d0b757723d1bb3047ee354fec212cbd
alejandro1403 <65137750+alejandro1403@users.noreply.github.com>
2022-03-17 18:29:46 -0500

    Update User.js

--- Fronted/src/pages/User.js
+++ Fronted/src/pages/User.js
@@ -191,7 +191,6 @@ export default function User() {
                       const { id, username, rol, email } = row;
                       const sede = row.sede_nombre;
                       const status = row.is_active;
-                      console.log(status);
                       const isItemSelected = selected.indexOf(username) !== -1;
 
                       return (
